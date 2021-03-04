f92b6dc37abe1136eb6f54d13fc3aa6f97acc298
mariele <mariele_rbr@hotmail.com>
2021-03-04 11:28:45 -0300

    Listando Agendamentos

--- src/routes/appointments_routes.ts
+++ src/routes/appointments_routes.ts
@@ -5,6 +5,13 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository'
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository;
 
+// SoC: Separation of Concerns (Separação de preocupações)
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+})
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
